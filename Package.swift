13277e3167475649fbdd20b73469de5f1811150c
Oleksandr Bretsko <bretsko.oleksandr@gmail.com>
2019-10-27 22:18:25 +0200

    update

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -7,7 +6,7 @@ let package = Package(
     name: "Base",
     
     platforms: [
-        .macOS(.v10_12), .iOS(.v11), .tvOS(.v10), .watchOS(.v4)
+        .macOS(.v10_13), .iOS(.v13), .tvOS(.v10), .watchOS(.v4)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -16,9 +15,8 @@ let package = Package(
             targets: ["Base"]),
     ],
     dependencies: [
-
-        // .package(url: "../Log Github", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/bretsko/Logger", .upToNextMajor(from: "1.0.0")),
+        //.package(url: "https://github.com/bretsko/MinimalBase", .from: "1.0.0"),
+        .package(url: "https://github.com/bretsko/Log", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,8 +24,8 @@ let package = Package(
         .target(
             name: "Base",
             dependencies: ["Log"]), 
-        // .testTarget(
-        //     name: "BaseTests",
-        //     dependencies: ["Base"]),
+        .testTarget(
+            name: "BaseTests",
+            dependencies: ["Base"]),
     ]
 )
